ee28974d0562f66458780f906cd26540cbd62ec5
philippe <^1:schaeffer>
2020-04-13 22:58:26 +0200

    Ajout d'un second consumer pour test partition

---
+++ consumer2/src/main/java/org/cnam/kafka/consumer2/Consumer2Application.java
@@ -0,0 +1,66 @@
+package org.cnam.kafka.consumer2;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.cnam.kafka.consumer2.model.StationLocation;
+import org.cnam.kafka.consumer2.model.StationLocationDeserializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class Consumer2Application {
+    private static final String KAFKA_BROKER_URL = "127.0.0.1:9092";
+    private static final String TOPIC_NAME = "tp-topic";
+    private static final String GROUP_ID = "tp-group";
+    private List<StationLocation> stationLocationList = new ArrayList<>();
+
+    public Consumer2Application() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER_URL);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StationLocationDeserializer.class.getName());
+
+        KafkaConsumer<String, StationLocation> consumer = new KafkaConsumer<String, StationLocation>(properties);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+            ConsumerRecords<String, StationLocation> consumerRecords = consumer.poll(Duration.ofMillis(10));
+            consumerRecords.forEach(cr -> {
+                int countDiff = 0;
+                StationLocation currentStationLocation = cr.value();
+
+                if (!stationLocationList.contains(currentStationLocation)) {
+                    stationLocationList.add(currentStationLocation);
+                } else {
+                    StationLocation stored = stationLocationList.get(stationLocationList.indexOf(currentStationLocation));
+                    countDiff = stored.available_bikes - currentStationLocation.available_bikes;
+                }
+
+                if (countDiff != 0) {
+                    String chNbVelos = Math.abs(countDiff) > 1 ? "vélos disponibles" : "vélo disponible";
+                    System.out.println("Disponibilité : " + countDiff + " " + chNbVelos + " dans la station n° " + currentStationLocation.number + " à " + currentStationLocation.contract_name +", soit : "+ currentStationLocation.available_bikes + " disponible(s) sur " + currentStationLocation.bike_stands + " au total.");
+                }
+
+                System.out.println("CONSUMER2 :  Records : Key=>" + cr.key() + ", value=>" + cr.value() + ", offset=>" + cr.offset() + " partition=>"+ cr.partition());
+            });
+        }, 1000, 1000, TimeUnit.MILLISECONDS);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer2Application.class, args);
+    }
+
+}
---
+++ consumer2/src/main/java/org/cnam/kafka/consumer2/model/StationLocationDeserializer.java
@@ -0,0 +1,26 @@
+package org.cnam.kafka.consumer2.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+
+public class StationLocationDeserializer implements Deserializer {
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public StationLocation deserialize(String arg0, byte[] arg1) {
+        ObjectMapper mapper = new ObjectMapper();
+        StationLocation stationLocation = null;
+        try {
+            stationLocation = mapper.readValue(arg1, StationLocation.class);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return stationLocation;
+    }
+}
